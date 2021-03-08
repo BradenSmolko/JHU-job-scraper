3b29883b448dbeac8f2fc0f7ae867174849861d6
Braden Smolko <50993629+BradenSmolko@users.noreply.github.com>
2021-03-08 14:16:48 -0500

    Update Interactive Quick Search

--- QuickSearch.py
+++ QuickSearch.py
@@ -5,7 +5,10 @@ This script can be run to enter search terms manually and scrape all of the resu
 from JHUScraper import *
 
 search_terms = input('Enter your search terms and hit enter to run: ')
+print('\nGetting job information...')
 
 scraper = JHUScraper(search_terms=search_terms)
 scraper.scrape_all_results()
 scraper.jobs_to_csv()
+
+print('Done! File can be found in your current working directory.')
